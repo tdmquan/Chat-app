b67575c040c6097208cde634b356f7bfd43449b6
tdmquan <tdmquan77@gmail.com>
2025-02-05 12:09:15 +0700

    Complete project

---
+++ server/routes/AuthRoutes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { getUserInfo, signup, login, updateProfile, addProfileImage, removeProfileImage, logOut } from "../controllers/AuthController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
+import multer from "multer"
+
+const authRoutes = Router();
+const upload = multer({ dest: "uploads/profiles/" });
+
+authRoutes.post("/signup", signup);
+authRoutes.post("/login", login);
+authRoutes.get("/user-info", verifyToken, getUserInfo);
+authRoutes.post("/update-profile", verifyToken, updateProfile);
+authRoutes.post("/add-profile-image", verifyToken, upload.single("profile-image"), addProfileImage);
+authRoutes.delete("/remove-profile-image", verifyToken, removeProfileImage)
+authRoutes.post("/logout", logOut)
+
+export default authRoutes;
